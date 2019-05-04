e0e7551f6ad31e1d5805a470f215d95d1f6a42db
kuro525 <deep.impact525-2012-5-pc@hotmail.co.jp>
2019-05-05 02:11:21 +0900

    [Add] CameraComponent

---
+++ Camera.vue
@@ -0,0 +1,27 @@
+<template>
+  <video ref="video" id="video" width="400" height="400" playsinline muted autoplay></video>
+</template>
+
+<script>
+export default {
+  name: 'Camera',
+  data() {
+    return {
+      video: {}
+    };
+  },
+  mounted() {
+    this.video = this.$refs.video;
+    navigator.mediaDevices
+      .getUserMedia({ video: true, audio: false })
+      .then(stream => {
+        this.video.srcObject = stream;
+        this.video.play();
+      })
+      .catch(err => {
+        console.log('エラー! ' + err);
+      });
+    console.log(this.$refs.video);
+  }
+};
+</script>
